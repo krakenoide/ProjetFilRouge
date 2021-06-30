6413ac50f490393fbe69d54e63253e6aac1f30e0
AntoineWeberDB <weber.antoine.db@gmail.com>
2021-06-30 10:23:26 +0200

    trying git on PFR

---
+++ src/main/java/com/springmvc/modeles/PathVersRessourcesWeb.java
@@ -0,0 +1,20 @@
+package com.springmvc.modeles;
+
+public class PathVersRessourcesWeb {
+//	public static String path = "C:\\Users\\brice\\eclipse-workspace\\projetfilrouge\\src\\main\\webapp\\ressourcesweb";
+	public static String path = "C:\\Users\\Aewys\\Downloads\\projetfilrouge12_06\\projetfilrouge\\src\\main\\webapp\\ressourcesweb";
+//	public static String path = "C:\\Users\\Aewys\\tempPFR\\ressourcesweb";
+
+
+	public PathVersRessourcesWeb(){}
+
+	public static String getPath() {
+		return path;
+	}
+
+	public static void setPath(String path) {
+		PathVersRessourcesWeb.path = path;
+	}
+
+	
+}
