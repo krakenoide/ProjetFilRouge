f23537cf40b9fb203a3defc9cf6fb2153a702c58
AntoineWeberDB <weber.antoine.db@gmail.com>
2021-06-30 11:47:05 +0200

    Revert "removed files from git"
    This reverts commit 6f06b19748b55f21c69104c8bd38f0fd0d6cbf83.

---
+++ src/main/java/com/springmvc/modeles/PathVersRessourcesWeb.java
@@ -0,0 +1,19 @@
+package com.springmvc.modeles;
+
+public class PathVersRessourcesWeb {
+	
+	
+	public static String path = "C:/Users/Aewys/eclipse-workspace/projetfilrouge/src/main/webapp/ressourcesweb/";
+	
+
+	public PathVersRessourcesWeb(){}
+
+	public static String getPath() {
+		return path;
+	}
+
+	public static void setPath(String path) {
+		PathVersRessourcesWeb.path = path;
+	}
+	
+}
