0d8e986352bc7e3af6a350b04bf3b747df5b4169
krakenoide <guillaume.bortoli@gmail.com>
2021-06-30 11:13:04 +0200

    setting up path

--- src/main/java/com/springmvc/modeles/PathVersRessourcesWeb.java
+++ src/main/java/com/springmvc/modeles/PathVersRessourcesWeb.java
@@ -2,8 +2,8 @@ package com.springmvc.modeles;
 
 public class PathVersRessourcesWeb {
 	
-	public static String path = "C:/Users/Aewys/eclipse-workspace/projetfilrouge/src/main/webapp/ressourcesweb/";
-//	public static String path = "C:/Users/Guillaume/eclipse-workspace/projetfilrouge/src/main/webapp/ressourcesweb/";
+//	public static String path = "C:/Users/Aewys/eclipse-workspace/projetfilrouge/src/main/webapp/ressourcesweb/";
+	public static String path = "C:/Users/Guillaume/Desktop/Progra/Projets_Java/projetfilrouge/src/main/webapp/ressourcesweb/";
 //	public static String path = "C:/Users/Brice/eclipse-workspace/projetfilrouge/src/main/webapp/ressourcesweb/";
 //	public static String path = "C:/Users/Adrien/eclipse-workspace/projetfilrouge/src/main/webapp/ressourcesweb/";
 	
